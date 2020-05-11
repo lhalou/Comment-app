1d3ef431429b1083e793dcbb35d20367fce8b298
lliqi <614124284@qq.com>
2020-05-11 21:41:15 +0800

    显示评论发布时间

--- src/Comment.js
+++ src/Comment.js
@@ -4,6 +4,25 @@ class Comment extends Component {
   static propTypes = {
     comment: PropTypes.object.isRequired,
   };
+  constructor() {
+    super();
+    this.state = {
+      timeString: "",
+    };
+  }
+  componentWillMount() {
+    this._updateTimeString();
+  }
+  _updateTimeString() {
+    const comment = this.props.comment;
+    const duration = (+Date.now() - comment.createdTime) / 1000;
+    this.setState({
+      timeString:
+        duration > 60
+          ? `${Math.round(duration / 60)}分钟前`
+          : `${Math.round(Math.max(duration, 1))}秒前`,
+    });
+  }
   render() {
     return (
       <div className="comment">
@@ -11,6 +30,7 @@ class Comment extends Component {
           <span>{this.props.comment.username}</span>
         </div>
         <p>{this.props.comment.content}</p>
+        <span className="comment-createdTime">{this.state.timeString}</span>
       </div>
     );
   }
