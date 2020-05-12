f3783254be1f337c3155d21872878433e5bfc73d
lliqi <614124284@qq.com>
2020-05-12 15:57:54 +0800

    add

---
+++ build/precache-manifest.2ce822570206c2842c4560c0dbf19ec9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a0324b57c2a065135f6547c98cf2d6c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "586b82bf73b02627412a",
+    "url": "./static/css/main.88d89887.chunk.css"
+  },
+  {
+    "revision": "983da7e225b44b2ec606",
+    "url": "./static/js/2.a000a4fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a000a4fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "586b82bf73b02627412a",
+    "url": "./static/js/main.0805ea60.chunk.js"
+  },
+  {
+    "revision": "0c47c88f197fa1b0eee6",
+    "url": "./static/js/runtime-main.ca8fd237.js"
+  }
+]);
